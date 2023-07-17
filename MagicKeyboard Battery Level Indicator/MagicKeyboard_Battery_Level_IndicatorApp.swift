323cff1e67b4b7d45c3bc2d5921057865d1284b7
Jordan <henriojord@gmail.com>
2023-07-17 15:26:13 +0900

    Initial Commit

---
+++ MagicKeyboard Battery Level Indicator/MagicKeyboard_Battery_Level_IndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicKeyboard_Battery_Level_IndicatorApp.swift
+//  MagicKeyboard Battery Level Indicator
+//
+//  Created by Jordan Henrio on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct MagicKeyboard_Battery_Level_IndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
